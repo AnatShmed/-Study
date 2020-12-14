7a17178a2f0d552341ceb1180478a185c87c5bf9
AnatShmed <72926235+AnatShmed@users.noreply.github.com>
2020-12-14 15:49:48 +0200

    Update ThirdFunction.java

--- ThirdFunction.java
+++ ThirdFunction.java
@@ -1,14 +1,12 @@
 public class ThirdFunction {
 	public static int digitalRoot(int n) {
 		if(n<10) return n;
-		while(n>9) {
-			int num=0;
-			while(n>0){
-				num+=n%10;
-				n=n/10;
-			}
-			n=num;
+		int num=0;
+		while(n>0){
+			num+=n%10;
+			n=n/10;
 		}
+		n=digitalRoot(num);
 		return n;
 	}
 	public static void main(String[] args) {
